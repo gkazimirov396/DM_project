53f87570197339901085f0ff3283a876b7771f9f
gkazimirov396 <grishakazim@gmail.com>
2024-04-21 01:06:08 +0300

    feat: Implement Graph and Random Graph Generation

---
+++ DM_project/Graph.cs
@@ -0,0 +1,98 @@
+﻿namespace DM_project;
+
+
+internal class Graph
+{
+    private int vertices;
+    private List<int>[] adjacencyList;
+
+    public Graph(int v)
+    {
+        vertices = v;
+        adjacencyList = new List<int>[v];
+
+        for (int i = 0; i < v; ++i)
+        {
+            adjacencyList[i] = new List<int>();
+        }
+    }
+    public static Graph GenerateRandomGraph(int size, double density)
+    {
+        Random random = new Random();
+        Graph graph = new Graph(size);
+
+        int edgesToAdd = (int)(size * (size - 1) * density / 2);
+
+        while (edgesToAdd > 0)
+        {
+            int u = random.Next(0, size);
+            int v = random.Next(0, size);
+
+            if (u != v && !graph.adjacencyList[u].Contains(v))
+            {
+                graph.AddEdge(u, v);
+                edgesToAdd--;
+            }
+        }
+
+        return graph;
+    }
+
+    public void AddEdge(int u, int v)
+    {
+        adjacencyList[u].Add(v);
+        adjacencyList[v].Add(u);
+    }
+
+    public void RemoveEdge(int u, int v)
+    {
+        adjacencyList[u].Remove(v);
+        adjacencyList[v].Remove(u);
+    }
+
+    public void PrintAdjacencyList()
+    {
+        for (int i = 0; i < vertices; ++i)
+        {
+            Console.Write($"Vertex {i}: ");
+
+            foreach (int vertex in adjacencyList[i])
+            {
+                Console.Write($"{vertex} ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+
+    public void PrintAdjacencyMatrix()
+    {
+        int[,] adjacencyMatrix = GetAdjacencyMatrix();
+
+        for (int i = 0; i < vertices; i++)
+        {
+            for (int j = 0; j < vertices; j++)
+            {
+                Console.Write($"{adjacencyMatrix[i, j]} ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+
+    private int[,] GetAdjacencyMatrix()
+    {
+        int[,] matrix = new int[vertices, vertices];
+
+        for (int i = 0; i < vertices; ++i)
+        {
+            foreach (int vertex in adjacencyList[i])
+            {
+                matrix[i, vertex] = 1;
+            }
+        }
+
+        return matrix;
+    }
+}
+
---
+++ DM_project/Program.cs
@@ -0,0 +1,18 @@
+﻿using DM_project;
+
+Graph g = new Graph(5);
+g.AddEdge(0, 1);
+g.AddEdge(0, 2);
+g.AddEdge(1, 2);
+g.AddEdge(1, 3);
+g.AddEdge(2, 4);
+
+Console.WriteLine("Adjacency Lists:");
+g.PrintAdjacencyList();
+
+Console.WriteLine("\nAdjacency Matrix:");
+g.PrintAdjacencyMatrix();
+
+Graph randomGraph = Graph.GenerateRandomGraph(6, 1);
+Console.WriteLine("\nRandom Adjacency Lists:");
+randomGraph.PrintAdjacencyList();
